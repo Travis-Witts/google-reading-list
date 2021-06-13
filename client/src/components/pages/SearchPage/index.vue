f694743fa20a67347fd00cf9dd954645b17d4244
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-06-13 16:55:26 +0930

    chore: adding vue components and pages

---
+++ client/src/components/pages/SearchPage/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import './style.css'
+
+export default {
+  name: 'SearchPage',
+  components: {
+    SearchBar,
+    List
+  }
+}
+</script>
